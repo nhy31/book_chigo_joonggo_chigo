3912c53d7f0b07035984d60fbb966a1fa8d18686
nhy31 <nahay131@gmail.com>
2022-01-13 00:38:36 +0900

    프로젝트

--- bookchigo_0617/src/main/java/com/bookchigo/dao/GroupPurchaseDao.java
+++ bookchigo_0617/src/main/java/com/bookchigo/dao/GroupPurchaseDao.java
@@ -1,5 +1,4 @@
 package com.bookchigo.dao;
-// 2022test...
 
 import java.util.List;
 
@@ -10,33 +9,33 @@ import com.bookchigo.domain.GroupPurchase;
 
 public interface GroupPurchaseDao {
 	
-	// 리스트 불러오는거
+	// 由ъ뒪�듃 遺덈윭�삤�뒗嫄�
 	List<GroupPurchase> getGroupPurchaseList() throws DataAccessException;
 	List<GroupPurchase> getGroupPurchaseImReaderList(int readerMemberId) throws DataAccessException;
 	List<GroupPurchase> Search_GroupPurchase(String book_name) throws DataAccessException; 
 	List<GpApply> getWhoApplierList(int gp_postId) throws DataAccessException;
 	List<GpApply> getMyApplyList(int memberId) throws DataAccessException;
 
-	// get (얻어오는 작업 관련)
+	// get (�뼸�뼱�삤�뒗 �옉�뾽 愿��젴)
 	GroupPurchase getGpPostByPostId (int gp_postId) throws DataAccessException;
 
-	// 게시글 업로드와 삭제
+	// 寃뚯떆湲� �뾽濡쒕뱶�� �궘�젣
 	GroupPurchase Upload_GroupPurchase(GroupPurchase newPost) throws DataAccessException;
 	void Delete_GroupPurchase(int gp_postId) throws DataAccessException;
 	GroupPurchase Update_GroupPurchase(GroupPurchase Post) throws DataAccessException;
 	
-	// 자세히보기
+	// �옄�꽭�엳蹂닿린
 	GroupPurchase Detail_GroupPurchase(int gp_postId) throws DataAccessException;
 
-	// 공구 신청&취소
+	// 怨듦뎄 �떊泥�&痍⑥냼
 	void Apply_GroupPurchase(int gp_postId) throws DataAccessException; 
 	void Cancel_GroupPurchase(int gp_postId) throws DataAccessException; 
 	
-	// 공구신청취소 -> 디비에 삽입삭제, 신청목록에 삽입삭제
+	// 怨듦뎄�떊泥�痍⑥냼 -> �뵒鍮꾩뿉 �궫�엯�궘�젣, �떊泥�紐⑸줉�뿉 �궫�엯�궘�젣
 	void insertGpApply(GpApply newApply) throws DataAccessException; 
 	void deleteGpApply(GpApply delApply) throws DataAccessException; 
 	
-	// ing 체크 및 UPDATE
+	// ing 泥댄겕 諛� UPDATE
 	void ingUpdate(int gp_postId) throws DataAccessException; 
 	
 }
